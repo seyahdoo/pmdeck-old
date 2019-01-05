dae711fd7d604cb08c33b2b4277494fd61b454a4
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2019-01-06 01:49:47 +0300

    init

--- Python/deck.py
+++ Python/deck.py
@@ -1,17 +1,73 @@
 
+import socket
+import threading
+
+
 class DeviceManager:
 
-    decks = []
+    def __init__(self):
 
-    def find_all(self):
+        self.connected_callback = None
+
+        threading.Thread(
+            target=self.connector_listener
+        ).start()
+
+        return
+
+    def connector_listener(self):
+        bind_ip = '0.0.0.0'
+        bind_port = 23997
+
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((bind_ip, bind_port))
+        server.listen(5)  # max backlog of connections
+        print('Listening on {}:{}'.format(bind_ip, bind_port))
+
+        while True:
+            client_sock, address = server.accept()
+            print('Accepted connection from {}:{}'.format(address[0], address[1]))
+            client_handler = threading.Thread(
+                target=self.deck_listener,
+                args=(client_sock,)
+                # without comma you'd get a... TypeError: handle_client_connection() argument after * must be a sequence, not _socketobject
+            )
+            client_handler.start()
+
+        return
+
+    def deck_listener(self, client_socket):
+
+        deck = Deck()
+        self.on_connected(deck)
+
+        while True:
+            try:
+                data = client_socket.recv(1024)
+                msg = data.decode('utf-8')
+                cmd = msg.split(';')
+                deck.on_key_status_change(cmd[0], cmd[1])
+            except Exception as e:
+                print(e)
+                self.on_disconnected(deck)
+                return
+
+        return
+
+    def set_on_connected_callback(self, callback):
+        self.connected_callback = callback
         return
 
     def on_connected(self, deck):
+        print("Connected")
+        self.connected_callback()
         return
+
+
 
     def on_disconnected(self, deck):
+        print("Disconnected")
         return
-
 
 
 class Deck:
@@ -27,7 +83,9 @@ class Deck:
 
     KEY_IMAGE_SIZE = KEY_PIXEL_WIDTH * KEY_PIXEL_HEIGHT * KEY_PIXEL_DEPTH
 
-    def __init__(self):
+    def __init__(self, socket):
+
+        self.socket = socket;
 
         return
 
@@ -51,4 +109,7 @@ class Deck:
 
         return
 
+    def on_key_status_change(self, key, status):
+        print("Key Status {}, {}".format(key, status));
+        return
 
---
+++ Python/main.py
@@ -0,0 +1,14 @@
+
+import deck
+from threading import Event
+
+
+
+
+
+if __name__ == "__main__":
+
+    manager = deck.DeviceManager()
+
+
+    Event().wait()
